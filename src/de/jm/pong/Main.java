d2a0e52ea551d661b18aa8694e4cbf006b27a10f
petabyteboy <petabyteboy@gmail.com>
2014-09-11 18:04:19 +0200

    DONE MOAR STAFF

--- src/de/jm/pong/Main.java
+++ src/de/jm/pong/Main.java
@@ -1,5 +1,14 @@
 package de.jm.pong;
 
-public class Main {
 
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Pong game = new Pong();
+		
+		game.startGame();
+		
+	}
+	
 }
--- src/de/jm/pong/Pong.java
+++ src/de/jm/pong/Pong.java
@@ -1,5 +1,33 @@
 package de.jm.pong;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
 public class Pong {
+
+	public void startGame() {
+		try {
+			Display.setDisplayMode(new DisplayMode (640, 480));
+			Display.setVSyncEnabled(true);
+			Display.setTitle("Pong");
+			Display.create();
+			
+			Keyboard.create();
+		} catch (LWJGLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		while (!Display.isCloseRequested()) {
+			
+			
+			
+		    Display.update();
+		}
+		
+		Display.destroy();
+	}
 
 }
